979f0538ba34b35d25b80441bdfe50a42771b7cc
midhuna-21 <krishnamidhuna850@gmail.com>
2025-12-22 11:51:02 +0530

    config set up modified

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 const nextConfig: NextConfig = withBundleAnalyzer({
   compress: true,
   productionBrowserSourceMaps: true,
+   trailingSlash: true,
 
 
   experimental: {
