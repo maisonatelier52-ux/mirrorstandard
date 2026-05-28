83ac11c203160c130c04edce11a48136c2a93d55
news structure <maisonatelier52@gmail.com>
2026-05-28 11:24:04 +0530

    fixed the error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Oswald } from "next/font/google";
-import "./globals.css";
 
-import { Providers } from '../components/ThemeProvider'
 import { Roboto } from 'next/font/google';
 import Script from "next/script";
 import { getLatestNewsByCategory } from "../lib/news";
